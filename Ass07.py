704e5780deeaec6276d75c23ded3a493ab4e31fb
kmnguyen37 <kmnguyen37@gmail.com>
2017-05-09 16:14:08 -0500

    Add to existing file

--- Ass07.py
+++ Ass07.py
@@ -1,16 +1,16 @@
 import sys
 
-#usage = sys.argv[0] + ": genome.fasta features.gff"
+usage = sys.argv[0] + ": genome.fasta features.gff"
 
-#if len(sys.argv) < 3:
- #   print(usage)
-  #  sys.exit("\nThis script requires both a FASTA file and a GFF file\n")
+if len(sys.argv) < 3:
+    print(usage)
+    sys.exit("\nThis script requires both a FASTA file and a GFF file\n")
 
-#genome = sys.argv[1]
-#gff    = sys.argv[2]
+genome = sys.argv[1]
+gff    = sys.argv[2]
 
-#data1 = open(genome, 'r')
-#data2 = open(gff, 'r')
+data1 = open(genome, 'r')
+data2 = open(gff, 'r')
 
 # Read FASTA file
 data1 = open('watermelon.fsa', 'r')
